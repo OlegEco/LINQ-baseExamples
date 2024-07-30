6db5ab740ab5e345d1b746f78947878294443bea
Oleg <100606637+OlegEco@users.noreply.github.com>
2024-07-30 11:20:43 +0200

    LinqToSQL its old. Its only from Framewrok. Preference EFCore

---
+++ InterpretieredQueries/Program.cs
@@ -0,0 +1,55 @@
+﻿//using System;
+//using System.ComponentModel.DataAnnotations.Schema;
+//using System.Data.Linq;
+//using System.Data.Linq.Mapping;
+//using System.Linq;
+
+//[Table(Name = "Customers")]
+//public class Customer
+//{
+//    [Column(IsPrimaryKey = true, IsDbGenerated = true)]
+//    public int ID { get; set; }
+
+//    [Column]
+//    public string Name { get; set; }
+//}
+
+//class Test
+//{
+//    static void Main()
+//    {
+//        var dataContext = new DataContext(@"Server=(localdb)\MSSQLLocalDB;Database=MyDatabase;Trusted_Connection=True;");
+
+//        Table<Customer> customers = dataContext.GetTable<Customer>();
+
+//        IQueryable<string> query = from c in customers
+//                                   where c.Name.Contains("a")
+//                                   orderby c.Name.Length
+//                                   select c.Name.ToUpper();
+
+//        foreach (string name in query)
+//        {
+//            Console.WriteLine(name);
+//        }
+//    }
+//}
+
+
+
+
+//IQueryable<string> queryExample = customer
+//    .Where(x => x.Name.Contains("a"))
+//    .OrderBy(x => x.Name.Length)
+//    .Select(x => x.Name.ToUpper());
+
+
+
+//using System.Text.RegularExpressions;
+
+//Regex wordCounter = new Regex(@"\Ь(\w | [-'])+\Ь");
+//var query = dataContext.MedicalArticles
+//.Where(article => article.Topic == "influenza")
+//.AsEnumeraЬle()
+//.Where(article =>
+//wordCounter.Matches(article.AЬstract).Count
+//, 100);
